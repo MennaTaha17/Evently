813000a68e5cc6fb0856aa9b51319e2730629003
MennaTaha17 <elshreeftaha79@gmail.com>
2025-09-29 22:56:37 +0300

    app theme added

---
+++ lib/common/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color mainColor = Color(0xff5669FF);
+  static const Color darkBgColor = Color(0xff101127);
+  static const Color lightBgColor = Color(0xffF2FEFF);
+  static const Color darkTextColor = Color(0xffF4EBDC);
+  static const Color lightTextColor = Color(0xff1C1C1C);
+  static const Color redColor = Color(0xffFF5659);
+  static const Color grayColor = Color(0xff7B7B7B);
+}
